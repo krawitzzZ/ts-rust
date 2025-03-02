8cedf2ff72150284dfed86058cea55919a3fc979
Nikita Demin <krawitzzz@hotmail.com>
2025-03-02 17:44:45 +0100

    test: Configure coverage

--- packages/std/jest.config.js
+++ packages/std/jest.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   testEnvironment: "node",
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
+  coverageThreshold: {
+    global: {
+      statements: 85,
+      branches: 80,
+      functions: 90,
+      lines: 85,
+    },
+  },
+  coverageReporters: ["text", "lcov"],
+  coverageDirectory: "coverage",
 };
