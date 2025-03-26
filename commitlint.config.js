22ff78a55ae5f7b595bbf3016750df6d7d3ad028
Nikita Demin <krawitzzz@hotmail.com>
2025-03-26 23:24:32 +0100

    chore: Setup husky and pre-commit hooks

---
+++ commitlint.config.js
@@ -0,0 +1,30 @@
+/** @type {import('@commitlint/types').UserConfig} */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [2, "always", ["shared", "std", "docs", "ci", "tests"]],
+    "scope-case": [2, "always", "lowercase"],
+    "subject-case": [2, "always", "sentence-case"],
+    "subject-max-length": [2, "always", 100],
+    "type-case": [2, "always", "lower-case"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "doc",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+        "tests",
+      ],
+    ],
+  },
+};
