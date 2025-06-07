4bad437a8fffa9f8682219c571997a6d6e4c16f8
Nikita Demin <krawitzzz@hotmail.com>
2025-06-07 15:44:44 +0200

    test: Switch to swc/jest

--- packages/std/src/result/types.ts
+++ packages/std/src/result/types.ts
@@ -1,5 +1,12 @@
 import { Result } from "./index";
 
-// TODO(nikita.demin): check if needed
+// TODO(nikita.demin): also check pending result in infer
 export type OkValue<R, E = unknown> = R extends Result<infer U, E> ? U : never;
 export type ErrValue<R, V = unknown> = R extends Result<V, infer E> ? E : never;
+
+export type OkValues<T extends readonly Result<unknown, unknown>[]> = {
+  [idx in keyof T]: T[idx] extends Result<infer U, unknown> ? U : never;
+};
+export type ErrValues<T extends readonly Result<unknown, unknown>[]> = {
+  [idx in keyof T]: T[idx] extends Result<unknown, infer U> ? U : never;
+};
